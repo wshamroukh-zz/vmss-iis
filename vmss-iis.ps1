ffb9c93ae9197dacd72bed3d88212a6e6abd9f42
wshamroukh <waddahs@microsoft.com>
2019-03-27 17:40:28 +0400

    Update vmss-iis.ps1

--- vmss-iis.ps1
+++ vmss-iis.ps1
@@ -2,7 +2,7 @@ Add-WindowsFeature Web-Server
 Add-Content -Path "C:\inetpub\wwwroot\Default.htm" -Value $($env:computername)
 New-Item -ItemType directory -Path "C:\inetpub\wwwroot\sahara"
 New-Item -ItemType directory -Path "C:\inetpub\wwwroot\safari"
-$saharavalue = "Sahara: GW-01-" + $($env:computername)
+$saharavalue = "Sahara: GW-02-" + $($env:computername)
 Add-Content -Path "C:\inetpub\wwwroot\sahara\test.htm" -Value $saharavalue
-$safaravalue = "Safari: GW-01-" + $($env:computername)
+$safaravalue = "Safari: GW-02-" + $($env:computername)
 Add-Content -Path "C:\inetpub\wwwroot\safari\test.htm" -Value $safaravalue
