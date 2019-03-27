4f42a83171be87681df9ca48a312f039ea719743
wshamroukh <waddahs@microsoft.com>
2019-03-27 17:29:04 +0400

    Update vmss-iis.ps1

--- vmss-iis.ps1
+++ vmss-iis.ps1
@@ -1,7 +1,7 @@
 ﻿Add-Content -Path "C:\inetpub\wwwroot\Default.htm" -Value $($env:computername)
 New-Item -ItemType directory -Path "C:\inetpub\wwwroot\sahara"
 New-Item -ItemType directory -Path "C:\inetpub\wwwroot\safari"
-$saharavalue = "Sahara: GW-02-" + $($env:computername)
+$saharavalue = "Sahara: GW-01-" + $($env:computername)
 Add-Content -Path "C:\inetpub\wwwroot\sahara\test.htm" -Value $saharavalue
-$safaravalue = "Safari: GW-02-" + $($env:computername)
+$safaravalue = "Safari: GW-01-" + $($env:computername)
 Add-Content -Path "C:\inetpub\wwwroot\safari\test.htm" -Value $safaravalue
